92cc84c7ee64a9724142b70bfadb1358afe63b62
Slava Krokhalev <krox1337@yandex.ru>
2018-11-18 18:19:47 +0500

    base Game settings

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ coder = 'Slava Krokhalev'
 designer = 'Sasha Vasin'
 
 size = (width, height) = (1200, 600)
+
+white = (255, 255, 255)
+black = (0, 0, 0)
--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 import pygame
 import config
 from pygame.locals import *
+
+pygame.init()
+
+
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode(config.size)
+        self.screen_rect = self.screen.get_rect()
+        self.clock = pygame.time.Clock()
+        self.is_running = True
+
+    def handler(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                self.is_running = False
+
+    def draw(self):
+        self.screen.fill(config.white)
+
+    def run(self):
+        while self.is_running:
+            self.handler()
+            self.draw()
+            pygame.display.update()
+            self.clock.tick(60)
+
+game = Game()
+
+if __name__ == '__main__':
+    game.run()

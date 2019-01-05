5526b1fb909221232de4e2e0812fc7f83473e278
coffeecoder <krox1337@yandex.ru>
2019-01-05 21:37:11 +0500

    init creator

---
+++ level_creator.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.locals import *
+import config
+
+
+class Creator:
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def move(self):
+        pass
+
+    def save_level(self):
+        pass
+
+    def load_level(self):
+        pass

5d6e0b4ab24be72b63044162410d18309da9f911
coffeecoder <krox1337@yandex.ru>
2019-01-05 21:51:18 +0500

    set default settings

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ coder = 'Slava Krokhalev'
 designer = 'Sasha Vasin'
 
 size = (width, height) = (1200, 600)
+creator_size = (width, height) = (1200, 600)
 
 white = (255, 255, 255)
 black = (0, 0, 0)
--- level_creator.py
+++ level_creator.py
@@ -1,11 +1,28 @@
 import pygame
 from pygame.locals import *
+import menu
+import player
+import platforms
 import config
 
 
 class Creator:
     def __init__(self):
-        pass
+        pygame.display.set_caption("MiniBrains | Level Creator")
+
+        # screen
+        self.screen = pygame.display.set_mode(config.creator_size)
+        self.screen_rect = self.screen.get_rect()
+        self.clock = pygame.time.Clock()
+        self.is_running = True
+
+        # objects
+        self.all_objects = pygame.sprite.Group()
+        self.platforms_group = pygame.sprite.Group()
+        self.menu = menu.Menu()
+
+        # add objects
+        self.all_objects.add(self.menu)
 
     def add(self):
         pass
@@ -21,3 +38,26 @@ class Creator:
 
     def load_level(self):
         pass
+
+    def handler(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                self.is_running = False
+
+    def draw(self):
+        self.screen.fill(config.white)
+        self.all_objects.draw(self.screen)
+        self.menu.items.draw(self.screen)
+
+    def run(self):
+        while self.is_running:
+            self.handler()
+            self.draw()
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+creator = Creator()
+
+if __name__ == '__main__':
+    creator.run()

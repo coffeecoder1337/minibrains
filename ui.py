9e29ca0ec4094331a51c88e76378d57ba7db056b
coffeecoder <krox1337@yandex.ru>
2019-05-03 19:42:52 +0500

    added basic function

--- ui.py
+++ ui.py
@@ -3,10 +3,22 @@ from pygame.locals import *
 
 class Ui:
     def __init__(self):
-        pass
+        self.visible = True
     
     def show(self):
-        pass
+        self.visible = True
+        while self.visible:
+            self.loop()
     
     def hide(self):
+        self.visible = True
+
+    def draw(self):
         pass
+    
+    def handle(self):
+        pass
+    
+    def loop(self):
+        self.handle()
+        self.draw()
